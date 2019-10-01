fc5e3caba17faffa6b3d8e6735e81bb6aa17400a
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2019-10-01 13:06:54 +0300

    добавил js , меняю котов на плачущего кота , когда блок поворачивается стороной goog by

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let arrImg = [
+let arrImg1 = [
   '/img/iconfinder_096-grinning-cat-face_3099456.png',
   '/img/iconfinder_100-cat-face-with-wry-smile_3099452.png',
-  '/img/iconfinder_103-crying-cat-face_3099449.png',
+
   '/img/iconfinder_cat_285654.png',
 ];
-
+let arrImg2 = ['/img/iconfinder_103-crying-cat-face_3099449.png'];
 let but = document.querySelector('button');
 let boxLeft = document.querySelector('.box-anim-left');
 let boxRight = document.querySelector('.box-anim-right');
@@ -29,12 +29,19 @@ but.addEventListener('click', function(e) {
 setInterval(changeBackground, 1000);
 
 function changeBackground() {
-  boxLeft.style.backgroundImage = `url(${arrImg[i]})`;
-  boxRight.style.backgroundImage = `url(${arrImg[i]})`;
-
+  let h = Math.round(boxLeft.getBoundingClientRect().top);
+  console.log(h);
+  if (h < 0) {
+    console.log(1);
+    boxRight.style.backgroundImage = `url(${arrImg2[0]})`;
+    boxLeft.style.backgroundImage = `url(${arrImg2[0]})`;
+  } else {
+    boxLeft.style.backgroundImage = `url(${arrImg1[i]})`;
+    boxRight.style.backgroundImage = `url(${arrImg1[i]})`;
+  }
   i++;
 
-  if (arrImg[i] == undefined) {
+  if (arrImg1[i] == undefined) {
     i = 0;
   }
 }
